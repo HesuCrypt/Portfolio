13b2d4fbd3e19e0c1d19a1790c81ab9c2397a66f
HesuCrypt <136266903+HesuCrypt@users.noreply.github.com>
2025-06-16 15:27:23 +0800

    Update configuration and fix white screen issue

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,14 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/resume/',
+  base: './',
+  server: {
+    port: 5173,
+    strictPort: true,
+  },
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    sourcemap: true,
   },
   optimizeDeps: {
     exclude: ['lucide-react'],
